4a903b7410dfcf9775f34c599a5ba1944911b226
Vadim Kurilko <kurilkovadim@gmail.com>
2023-03-30 23:07:37 +0200

    Added lesson_8

---
+++ lesson_8/task_3.py
@@ -0,0 +1,10 @@
+def make_operation(operator, *args):
+    if operator == "+":
+        result = sum(args)
+    elif operator == "-":
+        result = args[0] - sum(args[1:])
+    elif operator == "*":
+        result = 1
+        for num in args:
+            result *= num
+    print(result)
